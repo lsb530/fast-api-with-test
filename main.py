3f31aa7087b356529d094f735f58cabc0d7e7231
seung.bok <seungbok.lee@huinnoaim.com>
2024-01-19 18:58:35 +0900

    change port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routers.user_api import user
@@ -18,3 +19,6 @@ async def say_hello(name: str):
 app.include_router(user)
 app.include_router(ws)
 app.include_router(graphql_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=10000)
